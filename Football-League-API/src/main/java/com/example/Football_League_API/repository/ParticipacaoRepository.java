951e8cf649c49cc5dbb493fe23196dfddf303c31
TavinhoSilva06 <tavinho.augusto.amaral@gmail.com>
2026-09-23 16:04:22 -0300

    Corrigindo erro de falta de ParticipacaoRepository no commit de preparação de participações

--- Football-League-API/src/main/java/com/example/Football_League_API/repository/ParticipacaoRepository.java
+++ Football-League-API/src/main/java/com/example/Football_League_API/repository/ParticipacaoRepository.java
@@ -3,7 +3,11 @@ package com.example.Football_League_API.repository;
 import com.example.Football_League_API.entity.Participacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ParticipacaoRepository extends JpaRepository<Participacao, Long> {
+    List<Participacao> findByTemporadaId(Long temporadaId);
+    Optional<Participacao> findByTemporadaIdAndTimeId(Long temporadaId, Long timeId);
 }
